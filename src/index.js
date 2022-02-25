22c06486ce59160c94ab59986c92db4fd146e798
haba-sensei <72942765+haba-sensei@users.noreply.github.com>
2022-02-24 23:13:23 -0500

    haba_tester2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
-alert("holi");
+alert("holi2");
 
 (async function App() {
   const main = null || document.getElementById('main');
